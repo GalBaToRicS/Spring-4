77da65f7ae1d33061fe7b7709c1001ae0b967279
Игорь К <73175148+GalBaToRicS@users.noreply.github.com>
2025-10-20 20:55:59 +0300

    Готово

--- Task-5-master/src/main/java/hiber/MainApp.java
+++ Task-5-master/src/main/java/hiber/MainApp.java
@@ -1,8 +1,10 @@
 package hiber;
 
 import hiber.config.AppConfig;
+import hiber.model.Car;
 import hiber.model.User;
 import hiber.service.UserService;
+import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
@@ -10,15 +12,15 @@ import java.util.List;
 
 public class MainApp {
    public static void main(String[] args) throws SQLException {
-      AnnotationConfigApplicationContext context = 
-            new AnnotationConfigApplicationContext(AppConfig.class);
+      AnnotationConfigApplicationContext context =
+              new AnnotationConfigApplicationContext(AppConfig.class);
 
       UserService userService = context.getBean(UserService.class);
 
-      userService.add(new User("User1", "Lastname1", "user1@mail.ru"));
-      userService.add(new User("User2", "Lastname2", "user2@mail.ru"));
-      userService.add(new User("User3", "Lastname3", "user3@mail.ru"));
-      userService.add(new User("User4", "Lastname4", "user4@mail.ru"));
+      userService.add(new User("User1", "Lastname1", "user1@mail.ru",new Car("Audi",5)));
+      userService.add(new User("User2", "Lastname2", "user2@mail.ru",new Car("BMW",6)));
+      userService.add(new User("User3", "Lastname3", "user3@mail.ru",new Car("Toyota",7)));
+      userService.add(new User("User4", "Lastname4", "user4@mail.ru",new Car("Honda",8)));
 
       List<User> users = userService.listUsers();
       for (User user : users) {
@@ -26,9 +28,11 @@ public class MainApp {
          System.out.println("First Name = "+user.getFirstName());
          System.out.println("Last Name = "+user.getLastName());
          System.out.println("Email = "+user.getEmail());
+         System.out.println(user.getCar());
          System.out.println();
       }
 
+      System.out.println(userService.getUserbyCar("Audi",5).toString());
       context.close();
    }
 }
